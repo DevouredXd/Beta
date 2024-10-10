6ef11e96d963cce587dd7b0603af22863b560780
DevouredXd <mccjhon9@gmail.com>
2024-10-10 15:25:56 +0100

    Update Auto-discord-status-changer-beta.js
    Changed to manifest

--- Auto-discord-status-changer-beta.js
+++ Auto-discord-status-changer-beta.js
@@ -1,5 +1,14 @@
+// Plugin manifest
+const manifest = {
+    name: "Auto Status Changer",
+    author: "DevouredXd",
+    description: "Automatically changes Discord status at a safe rate.",
+    version: "1.0.0",
+    type: "plugin"
+};
+
 let statusInterval;
-const statuses = ["beta", "beta", "beta", "beta", "beta"];
+const statuses = ["beta", "beta", "beta", "beta", "beta"]; // Status messages
 
 function changeStatus() {
     const randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
@@ -21,7 +30,9 @@ function stopAutoStatusChange() {
     }
 }
 
-export default {
+// Exporting the functions and manifest for Vendetta
+module.exports = {
+    manifest,
     startAutoStatusChange,
     stopAutoStatusChange
 };
